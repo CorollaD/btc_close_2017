695123ed5ddbf6cfb6587f2e2750490edaa10ba3
CorollaD <1976007504@qq.com>
2019-12-02 22:27:08 +0800

    3

--- btc_close_20171.py
+++ btc_close_20171.py
@@ -38,3 +38,21 @@ line_chart.x_labels_major = dates[::N]
 close_log = [math.log10(_) for _ in close]
 line_chart.add('log收盘价', close_log)
 line_chart.render_to_file('收盘价折线图 (¥).svg')
+
+from itertools import groupby
+
+def draw_line(x_data, y_data, title, y_legend):
+    xy_map = []
+    for x, y in groupby(sorted(zip(x_data, y_data)), key=lambda _:_[0]):
+        y_list = [v for _, v in y]
+        xy_map.append([x, sum(y_list) / len(y_list)])
+        x_unique, y_mean = [*zip(*xy_map)]
+        line_chart = pygal.Line()
+        line_chart.title = title
+        line_chart.x_labels = x_unique
+        line_chart.add(y_legend, y_mean)
+        line_chart.render_to_file('收盘价月日均值(¥).svg')
+        return line_chart
+    idx_month = dates.index('2017-12-01')
+    line_chart_month = draw_line(months[:idx_month], close[:idx_month], '收盘价月日均值(¥)', '月日均值')
+    line_chart_month
